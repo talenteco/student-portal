50f9405f33394920fb93ffeac2b4408f50f70872
Utkarsh_Prashant <utkarsh.prashant@students.iiit.ac.in>
2026-08-16 10:32:11 +0530

    Added a function to help calculate average of all course grades

--- dashboard.cpp
+++ dashboard.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 map<string, int> grades = {{"DSA", 85}, {"OS", 78}, {"DBMS", 90}};
 
+double calculateAverage() {
+    int sum = 0;
+    for (auto &p : grades) sum += p.second;
+    return (double)sum / grades.size();
+}
+
 void printSummary() {
     cout << "Course Grades:\n";
     for (auto &p : grades) cout << p.first << ": " << p.second << endl;
