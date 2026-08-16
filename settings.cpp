f350b0cebf326670378fc1b935a5aab25c75d113
Utkarsh_Prashant <utkarsh.prashant@students.iiit.ac.in>
2026-08-16 10:47:07 +0530

    Added a check to detect if user input is insufficient

--- settings.cpp
+++ settings.cpp
@@ -15,7 +15,14 @@ void changeTheme(Settings &s, string newTheme) {
     s.theme = newTheme;
 }
 
-int main() {
+bool checkIfEmpty(int a){
+    if(a<2) return true;
+    return false;
+}
+
+int main(int argc,  char* argv[]) {
+    bool a = checkIfEmpty(argc);
+    if(a) cout<<"Incorrect number of arguments, retry"<<endl;
     Settings userSettings;
     cout << "Notifications: " << userSettings.notifications << ", Theme: " << userSettings.theme << endl;
     toggleNotifications(userSettings);
