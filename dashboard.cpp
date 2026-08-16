73b17a36b3d092bfa2dfe7eeadb90bf4c1849eee
Utkarsh_Prashant <utkarsh.prashant@students.iiit.ac.in>
2026-08-16 10:29:15 +0530

    Added a function to print the course grades

--- dashboard.cpp
+++ dashboard.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 map<string, int> grades = {{"DSA", 85}, {"OS", 78}, {"DBMS", 90}};
 
-int main() {
-    printSummary();
+void printSummary() {
+    cout << "Course Grades:\n";
+    for (auto &p : grades) cout << p.first << ": " << p.second << endl;
+    cout << "Average: " << calculateAverage() << endl;
+}
+
+int main(int argc, char* argv[]) {
+    if(argv[1])printSummary();
     return 0;
 }
