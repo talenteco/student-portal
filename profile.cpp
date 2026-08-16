bcc550cefcf504fd82579c0914d9f131648dc4f2
Utkarsh_Prashant <utkarsh.prashant@students.iiit.ac.in>
2026-08-16 09:37:17 +0530

    Added a Function to display student info depending on the arguments(student credentials) recieved

--- profile.cpp
+++ profile.cpp
@@ -6,3 +6,24 @@ struct Profile {
     string roll_no;
     string branch;
 };
+
+void displayProfile(Profile p) {
+    cout << "Name: " << p.name << "\nRoll No: " << p.roll_no << "\nBranch: " << p.branch << endl;
+}
+
+
+int main(int argc, char* argc) {
+    map<string, Profile> accountDetails={{"std_a",{"std_a", "1", "CSE"}},
+                                        {"std_b",{"std_b", "2", "CSIS"}},
+                                        {"std_c",{"std_c", "3", "VLSI"}}};
+
+
+    Profile student;
+    student = accountDetails[argc[1]];
+
+    displayProfile(student);
+    updateBranch(student, "CSE-AI");
+    cout << "\nAfter update:\n";
+    displayProfile(student);
+    return 0;
+}
