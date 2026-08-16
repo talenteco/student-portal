4ff9829ed74b1f56d73e44d6c42d797f20d8a8ca
Utkarsh_Prashant <utkarsh.prashant@students.iiit.ac.in>
2026-08-16 06:59:22 +0530

    Added helper function to conver char* to ints, so that STUDENT_IDS could be sorted

--- mergesort.cpp
+++ mergesort.cpp
@@ -3,11 +3,35 @@
 
 using namespace std;
 
-
-int main(int argc, char* argv[]){
+int charStarToInt(char *c){
     
 
-    vector <int> student_ID = {8,2,3,4,22,4,112,4,1};
+    int val =0 ;
+    while(*c){
+    
+    if(*c<'0'||*c>'9') return -1;
+
+    if(val > (INT_MAX-(*c - '0')) / 10){
+            return -1;
+        }
+    
+    val=val*10+(*c-'0');
+    c++;
+
+    }
+    
+    return val;
+}
+
+int main(int argc, char* argv[]){
+
+    vector <int> student_ID ;
+    while(argv){
+        student_ID.push_back(charStarToInt(argv));
+        argv++;
+    }
+
+    
 
     mergesort(student_ID, 0, student_ID.size()-1);
 
