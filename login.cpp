4b05b5316cca09572e70f6a612d78bb7a8bf3772
Utkarsh_Prashant <utkarsh.prashant@students.iiit.ac.in>
2026-08-16 09:09:35 +0530

    Implemented credential validation

--- login.cpp
+++ login.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+//dummy account creds
+map<string, string> stored_credentials={{"std_a","abc"},
+                                        {"std_b","123"},
+                                        {"std_c","pass@123"} };
+
+
 struct login{
     string name;
     string password;
@@ -12,5 +18,10 @@ struct login{
 
 int main(int argc, char* argv[]){
 
+    login student;
+    student.name = argv[1];
+    student.password = argv[2];
 
+    if(stored_credentials[student.name]!=student.password) cout<<"INCORRECT ID OR PASSWORD";
+    else cout<<"Logged in Successfully";
 }
